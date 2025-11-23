1ee304a4f2786867ea544e8d24cdcb69cf2fe92c
travisjupp <travisjupp@users.noreply.github.com>
2025-11-23 21:41:35 +0000

    Create object method for solving multiple selection

--- src/App.js
+++ src/App.js
@@ -3,15 +3,65 @@ import { letters } from "./data.js";
 import Letter from "./Letter.js";
 
 export default function MailClient() {
-  const [selectedId, setSelectedId] = useState(null);
+  // Original
+  // const [selectedId, setSelectedId] = useState(null);
+
+  // const selectedCount = 1;
 
   // TODO: allow multiple selection
-  const selectedCount = 1;
+  // Object method
+  const [selectedIds, setSelectedIds] = useState({ count: 0 });
+
+  // Array method
+  // const [selectedIds, setSelectedIds] = useState([]);
 
   function handleToggle(toggledId) {
+    // Original
+    // setSelectedId(toggledId);
+
     // TODO: allow multiple selection
-    setSelectedId(toggledId);
+
+    // Object method
+    const checkIdVal = selectedIds[toggledId];
+    let nextSelectedIdsState;
+    if (checkIdVal === undefined) {
+      nextSelectedIdsState = {
+        ...selectedIds,
+        count: selectedIds.count + 1,
+        [toggledId]: true,
+      };
+    } else {
+      if (checkIdVal) {
+        nextSelectedIdsState = {
+          ...selectedIds,
+          count: selectedIds.count + -1,
+          [toggledId]: false,
+        };
+      } else {
+        nextSelectedIdsState = {
+          ...selectedIds,
+          count: selectedIds.count + 1,
+          [toggledId]: true,
+        };
+      }
+    }
+    setSelectedIds(nextSelectedIdsState);
+
+    // Array method
+    // let nextIds;
+    // if (selectedIds.some((id) => id === toggledId)) {
+    // nextIds = selectedIds.filter((id) => id !== toggledId);
+    // } else {
+    // nextIds = [...selectedIds, toggledId];
+    // }
+    // setSelectedIds(nextIds);
   }
+
+  // Array method
+  // const selectedCount = selectedIds.length;
+
+  // Object method
+  const selectedCount = selectedIds.count;
 
   return (
     <>
@@ -22,8 +72,17 @@ export default function MailClient() {
             key={letter.id}
             letter={letter}
             isSelected={
+              // Original
+              // letter.id === selectedId
+
               // TODO: allow multiple selection
-              letter.id === selectedId
+              // Object method
+              selectedIds[letter.id] !== undefined
+                ? selectedIds[letter.id]
+                : false
+
+              // Array method
+              // selectedIds.some((id) => id === letter.id)
             }
             onToggle={handleToggle}
           />
